8b3a69af2d5e93a45c1caaa0d622f2b4231a12e8
Levi <levi.vanlangeveld@vinted.com>
2025-11-18 11:04:08 +0200

    Add final version of exercises chapter 18

--- 18_getting_started_with_django/exercises/pizzeria_app/pizzas/urls.py
+++ 18_getting_started_with_django/exercises/pizzeria_app/pizzas/urls.py
@@ -8,4 +8,10 @@ app_name = 'pizzas'
 urlpatterns = [
     # Home page
     path("", views.index, name="index"),
+
+    # Show all pizzas
+    path("pizzas/", views.pizzas, name="pizzas"),
+
+    # Detail page for a single pizza
+    path("pizzas/<int:pizza_id>/", views.pizza, name="pizza"),
 ]
