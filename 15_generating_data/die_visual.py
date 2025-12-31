8e92e015ea464976dcb63089908512a691ff7b6c
leviatvinted <levi.vanlangeveld@vinted.com>
2025-12-31 13:14:35 +0100

    Add chapter 15 coursework

---
+++ 15_generating_data/die_visual.py
@@ -0,0 +1,31 @@
+import pygal
+from die import Die
+
+# Create a D6 and D10.
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls, and store the result in a list.
+results = []
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and D10 50,000 times"
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+                 '13', '14', '15', '16']
+hist_x_title = "Result"
+hist_y_title = "Frequency of the Result"
+
+hist.add('D6 + D10', frequencies)
+hist.render_to_file('die_visual.svg')
